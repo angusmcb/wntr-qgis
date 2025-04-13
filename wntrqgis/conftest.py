bfd78e8abcee5cbd1b58ff3f80159560109ae369
angusmcb <git@zog.mozmail.com>
2025-04-13 11:10:19 +0700

    Check wntr installed before importing in doctests

--- wntrqgis/conftest.py
+++ wntrqgis/conftest.py
@@ -1,8 +1,16 @@
 # content of conftest.py
 import pytest
-import wntr
+
+
+@pytest.fixture(scope="session")
+def check_wntr():
+    from wntrqgis.dependency_management import WqDependencyManagement
+
+    WqDependencyManagement.ensure_wntr()
 
 
 @pytest.fixture(autouse=True)
-def add_wntr(doctest_namespace):
+def add_wntr(check_wntr, doctest_namespace):  # noqa: ARG001
+    import wntr
+
     doctest_namespace["wntr"] = wntr
