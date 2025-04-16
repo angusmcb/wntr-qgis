b53fa75e9bf4cd2c501954396a6d9ebff821b072
angusmcb <git@zog.mozmail.com>
2025-04-17 00:39:16 +0630

    Simplify test_elements

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -13,56 +13,16 @@ from wntrqgis.elements import (
 )
 
 
-@pytest.mark.parametrize("unit", list(FlowUnit))
-def test_flow_unit_friendly_name(unit):
-    assert unit.friendly_name, f"{unit.name} is missing a friendly_name"
+@pytest.mark.parametrize(
+    "enum",
+    [FlowUnit, HeadlossFormula, PumpTypes, InitialStatus, ValveType, ModelLayer, ResultLayer, ModelField, ResultField],
+)
+def test_friendly_name(enum):
+    for member in enum:
+        assert member.friendly_name, f"{member.name} is missing a friendly_name"
 
 
-@pytest.mark.parametrize("formula", list(HeadlossFormula))
-def test_headloss_formula_friendly_name(formula):
-    assert formula.friendly_name, f"{formula.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("pump_type", list(PumpTypes))
-def test_pump_types_friendly_name(pump_type):
-    assert pump_type.friendly_name, f"{pump_type.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("status", list(InitialStatus))
-def test_initial_status_friendly_name(status):
-    assert status.friendly_name, f"{status.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("valve_type", list(ValveType))
-def test_valve_type_friendly_name(valve_type):
-    assert valve_type.friendly_name, f"{valve_type.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("layer", list(ModelLayer))
-def test_model_layer_friendly_name(layer):
-    assert layer.friendly_name, f"{layer.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("layer", list(ResultLayer))
-def test_result_layer_friendly_name(layer):
-    assert layer.friendly_name, f"{layer.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("field", list(ModelField))
-def test_model_field_name_matches_value(field):
-    assert field.name.lower() == field.value.lower(), f"{field.name} != {field.value}"
-
-
-@pytest.mark.parametrize("field", list(ModelField))
-def test_model_field_friendly_name(field):
-    assert field.friendly_name, f"{field.name} is missing a friendly_name"
-
-
-@pytest.mark.parametrize("field", list(ResultField))
-def test_result_field_name_matches_value(field):
-    assert field.name.lower() == field.value.lower(), f"{field.name} != {field.value}"
-
-
-@pytest.mark.parametrize("field", list(ResultField))
-def test_result_field_friendly_name(field):
-    assert field.friendly_name, f"{field.name} is missing a friendly_name"
+@pytest.mark.parametrize("enum", [ModelField, ResultField])
+def test_field_name_matches_value(enum):
+    for field in enum:
+        assert field.name.lower() == field.value.lower()
