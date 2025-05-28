f38de75dddaecf50d27c690919d7d14cc30cc419
angusmcb <git@zog.mozmail.com>
2025-05-28 23:36:29 +0200

    trying to fix mock again

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,3 +1,5 @@
+from unittest.mock import ANY
+
 import pytest
 from qgis.core import (
     Qgis,
@@ -114,7 +116,13 @@ def test_load_inp_file_bad_inp(qgis_iface, patched_plugin, mocker, bad_inp, qgis
 
     patched_plugin.actions["load_inp"].trigger()
 
-    qgis_iface.messageBar.return_value.pushMessage.assert_called_once()
+    qgis_iface.messageBar.return_value.pushMessage.assert_called_with(
+        title="Error",
+        text="error reading .inp file: (Error 201) syntax error (%s), at line 330:\n   [FOO]",
+        showMore=ANY,
+        level=Qgis.MessageLevel.Critical,
+        duration=ANY,
+    )
     assert len(QgsProject.instance().mapLayers()) == 0
 
 
