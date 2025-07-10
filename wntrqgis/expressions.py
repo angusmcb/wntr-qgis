2ea63682dcb3316e3dc456730249428cd6a40d0a
angusmcb <git@zog.mozmail.com>
2025-07-10 18:07:48 +0200

    Add curve and pattern expressions

--- wntrqgis/expressions.py
+++ wntrqgis/expressions.py
@@ -2,9 +2,13 @@ import math
 
 from qgis.core import qgsfunction
 
+import wntrqgis.interface
+
+GROUP = "Water Network Tools for Resilience"
+
 
 # qgis 3.28 errors if 'feature' and 'parent' are not included
-@qgsfunction(group="Water Network Tools for Resilience")
+@qgsfunction(group=GROUP)
 def wntr_result_at_current_time(column, feature, parent, context):  # noqa ARG001
     """
     Gets an individual result for the specified parameter.
@@ -40,3 +44,57 @@ def wntr_result_at_current_time(column, feature, parent, context):  # noqa ARG00
     end_value = column[math.ceil(timestep)]
 
     return start_value + (timestep - math.floor(timestep)) * (end_value - start_value)
+
+
+@qgsfunction(group=GROUP)
+def wntr_check_pattern(pattern, feature, parent, context):  # noqa ARG001
+    """
+    Checks if the input is a valid pattern string.
+    A valid pattern string is a space-separated list of numbers, e.g. "1 2 3 4".
+    An empty string is also considered valid, as it means no pattern is defined.
+
+    If the input is valid, it returns True; otherwise, it returns False.
+
+    <h2>Example usage:</h2>
+    <ul>
+      <li>wntr_check_pattern('1 2 3 4') -> true</li>
+      <li>wntr_check_pattern('x y z') -> false</li>
+    </ul>
+    """
+
+    try:
+        pattern = wntrqgis.interface._Patterns.read_pattern(pattern)  # noqa: SLF001
+    except ValueError:
+        return False
+
+    if pattern is None:
+        return None
+    else:
+        return True
+
+
+@qgsfunction(group=GROUP)
+def wntr_check_curve(curve):
+    """
+    Checks if the input is a valid curve string.
+    A valid curve string is a list of tuples, e.g. "[(1,2), (3,4)]".
+    An empty string is also considered valid, as it means no curve is defined.
+    If the input is valid, it returns True; otherwise, it returns False.
+
+    <h2>Example usage:</h2>
+    <ul>
+      <li>wntr_check_curve('[(1,2), (3,4)]') -> true</li>
+      <li>wntr_check_curve('(1,2), (3,4)') -> true</li>
+      <li>wntr_check_curve('(1,2)') -> true</li>
+      <li>wntr_check_curve('(x,y)') -> false</li>
+    </ul>
+    """
+    try:
+        curve = wntrqgis.interface._Curves.read_curve(curve)  # noqa: SLF001
+    except wntrqgis.interface.CurveReadError:
+        return False
+
+    if curve is None:
+        return None
+    else:
+        return True
